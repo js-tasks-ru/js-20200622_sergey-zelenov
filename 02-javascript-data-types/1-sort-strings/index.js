2883d3cd08fa314069dbb6f3daead2cfd27981cb
Sergey Zelenov <mail@zelenov.su>
2020-06-26 18:26:23 +0200

    1st module 1task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,18 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'lowerAsc') {
+export function sortStrings(arr, param = 'asc') {
+  switch (param) {
+  case 'asc':
+    return makeSorting(arr, 1);
+  case 'desc':
+    return makeSorting(arr, -1);
+  default:
+    return makeSorting(arr, 1);
+  }
 
+  function makeSorting(array, direction) {
+    return [...array].sort((a, b) =>
+      direction * a.localeCompare(b, 'default', {caseFirst: 'upper'}));
+  }
 }
